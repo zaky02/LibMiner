7388072c52d7aba894c01daee017251e409bacd9
IFilella <isaacfilella@gmail.com>
2025-01-24 17:00:47 +0100

    finished clusteing the centroids and outputting the results

--- subset_clustering/clustering_centroids.py
+++ subset_clustering/clustering_centroids.py
@@ -1,33 +1,28 @@
 import os
 import sys
 import pickle
-from rdkit import Chem
+from rdkit import Chem, DataStructs
 from rdkit.Chem import rdFingerprintGenerator, AllChem
 from rdkit.DataStructs import TanimotoSimilarity
+from rdkit.DataStructs.cDataStructs import CreateFromBitString
 import numpy as np
 import bitbirch as bb
 import scipy as sp
 from multiprocessing import Pool
+import pandas as pd
 
-def centroid_clustering(batch_file):
-    """
-    OBJECTIVE:
-    - READ THE PKL DICTIONARY FILE
-    - CLUSTER USING BITBIRCH THE CENTROID FINGERPRINTS (VALUES)
-    - TRACEBACK THE DICTIONARY KEYS SO THAT AFTER CLUSTERING THE CENTROIDS
-      THE FINAL CLUSTER ID CAN BE ADDED AS A NEW COLUMN TO ALL MOLECULES 
-      THAT HAD THAT CENTROID IN THEIR INITIAL CLUSTER
-    - WE ALSO HAVE TO RETRIEVE THE ACTUAL CENTROIDS FROM THE CLUSTERS BY
-      APPLYING THE MAX_SEPARATION CODE OF BITBIRCH BUT RETRIEVING THE MOST 
-      SIMILAR MOLECULE TO THE CENTROID
-    """
-    files = np.genfromtxt(batch_file, dtype=str)
+def centroid_clustering(batch_centroids, outname):
+    
+    files = np.genfromtxt(batch_centroids, dtype=str)
+    
     total_fps = []
     total_ids = []
     for file in files:
-        partition = file.replace('.pkl','')
+        partition = os.path.basename(file.replace('.pkl',''))
+        
         with open(file, 'rb') as f:
             part_fps = pickle.load(f)
+        
         for cluster, fp in part_fps.items():
             total_fps.append(fp)
             total_ids.append((partition, cluster))
@@ -36,18 +31,43 @@ def centroid_clustering(batch_file):
     print(f'Total number of centroids to cluster: {len(total_fps)}')
 
     centroids_dict, clusters_labels = get_bitbirch_centroid_clusters(total_fps)
-    #print(centroids_dict)
-    #print(clusters_labels)
 
-    print(f'Saving the new centroids of the clustering of'
-            'centroids as centroids_clustering.pkl')
-    path = os.path.dirname(batch_file)
-    with open(path + '/centroids_clustering.pkl','wb') as f:
+    # Get rdkit centroids_dict
+    rdkit_centroid_dict = {}
+    for ccluster, np_fp in centroids_dict.items():
+        bitstring_fp = ''.join(map(str, np_fp))
+        rdkit_fp = CreateFromBitString(bitstring_fp) 
+        rdkit_centroid_dict[ccluster] = rdkit_fp
+
+    path = os.path.dirname(batch_centroids) + '/'
+    print(f'Saving the new centroids of the centroid clustering'
+            f' to {path}{outname}')
+    #with open(path + outname + '.pkl','wb') as f:
+    #    pickle.dump(centroids_dict, f)
+    with open(path + outname + '_rdkit.pkl','wb') as f:
         pickle.dump(centroids_dict, f)
+    
+    # Get a dictionary of dictionaries with [partition][p_clusterid] = [c_clusterid]
+    pcluster_to_ccluster = {}
+    for i, ccluster in enumerate(clusters_labels):
+        partition, pcluster = total_ids[i]
+        if partition not in pcluster_to_ccluster:
+            pcluster_to_ccluster[partition] = {}
+            pcluster_to_ccluster[partition][pcluster] = ccluster
+        else:
+            pcluster_to_ccluster[partition][pcluster] = ccluster
 
-    print(f'Adding the final cluster id to molecules on each partition')
+    print(f'Adding the centroid cluster id to molecules on each partition')
     for file in files:
-        print(file.replace('.pkl','.csv'))
+        partition = os.path.basename(file.replace('.pkl',''))
+        df = pd.read_csv(file.replace('.pkl','.csv'))
+        pclusters = df['partition_cluster'].to_list()
+        cclusters = []
+        for pcluster in pclusters:
+            ccluster = pcluster_to_ccluster[partition][pcluster]
+            cclusters.append(ccluster)
+        df['centroid_cluster'] = cclusters
+        df.to_csv(file.replace('.pkl','.csv')) 
 
 def get_object_memory(obj):
     mem = sys.getsizeof(obj)
@@ -86,12 +106,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script that clusters the centroid fingerprints stored in a pkl file.')
 
     requiredArguments = parser.add_argument_group('Required Arguments')
-    requiredArguments.add_argument('--batch_file',
-                                   help='File containing the pickle files each with the centroid fingerprints of its corresponding partition clustering',
+    requiredArguments.add_argument('--batch_centroids',
+                                   help='Batch file with the paths to the pickle files with the centroid fingerprints of its corresponding partition clustering',
+                                   required=True)
+    requiredArguments.add_argument('--outname',
+                                   help='Outname for pkl file of centroids of the centroid clustering',
                                    required=True)
     
     args = parser.parse_args()
 
-    batch_file = args.batch_file
+    batch_centroids = args.batch_centroids
+    outname = args.outname
 
-    centroid_clustering(batch_file)
+    centroid_clustering(batch_centroids, outname)
