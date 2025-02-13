f1973e66d74757fcf912d7aff28b76b5d60734c2
Zaky02 <isaac.soul@alumni.esci.upf.edu>
2025-02-13 10:14:07 +0100

    Modified code to store the real centroids in the original csv file instead of mathematical representations in a pickle file

--- subset_clustering/clustering_centroids.py
+++ subset_clustering/clustering_centroids.py
@@ -1,119 +1,84 @@
-import os
-import sys
-import pickle
-from rdkit import Chem, DataStructs
-from rdkit.Chem import rdFingerprintGenerator, AllChem
-from rdkit.DataStructs import TanimotoSimilarity
-from rdkit.DataStructs.cDataStructs import CreateFromBitString
 import numpy as np
-import bitbirch as bb
-import scipy as sp
-from multiprocessing import Pool
 import pandas as pd
+from functools import partial
+from multiprocessing import Pool
+from utils.bitbirch_clustering import get_bitbirch_clusters
 
-def centroid_clustering(batch_centroids, outname):
+def process_file(file):
+    df = pd.read_csv(file)
+    centroid_df = df[df['partition_centroid'] == 1].copy()
+    centroid_df['source'] = file
+    return centroid_df
+
+def update_file(file, centroid_df):
+
+    print(f'Updating {file} with new centroid clusters and centroids:')
+    df = pd.read_csv(file)
+
+    file_centroids = centroid_df[centroid_df['source'] == file]
+    df = pd.merge(df, file_centroids, on=['ID', 'SMILES', 'dockscore', 'partition_cluster', 'partition_centroid'])
+
+    print(f'Mapping centroid cluster and centroid for file: {file}')
+    for cluster_id in file_centroids['centroid_cluster'].unique():
+        if pd.notna(cluster_id):
+            cluster_rows = file_centroids[file_centroids['centroid_cluster'] == cluster_id]
+            df.loc[(df['partition_cluster'].isin(cluster_rows['partition_cluster'])) &
+                    (df['partition_centroid'] == 0), 'centroid_cluster'] = int(cluster_id)
+            df.loc[(df['partition_cluster'].isin(cluster_rows['partition_cluster'])) &
+                    (df['partition_centroid'] == 0), 'centroid_centroid'] = int(0)
     
-    files = np.genfromtxt(batch_centroids, dtype=str)
+    if 'source' in df.columns:
+        df = df.drop(columns=['source'])
+
+    df.to_csv(file, index=False)
+    print(f'Updating for file: {file}, complete!')
+
+    return file
+
+def centroid_clustering(batch_partitions, num_workers):
     
-    total_fps = []
-    total_ids = []
-    for file in files:
-        partition = os.path.basename(file.replace('.pkl',''))
-        
-        with open(file, 'rb') as f:
-            part_fps = pickle.load(f)
-        
-        for cluster, fp in part_fps.items():
-            total_fps.append(fp)
-            total_ids.append((partition, cluster))
-     
-    total_fps = np.array(total_fps)
-    print(f'Total number of centroids to cluster: {len(total_fps)}')
-
-    centroids_dict, clusters_labels = get_bitbirch_centroid_clusters(total_fps)
-
-    # Get rdkit centroids_dict
-    rdkit_centroid_dict = {}
-    for ccluster, np_fp in centroids_dict.items():
-        bitstring_fp = ''.join(map(str, np_fp))
-        rdkit_fp = CreateFromBitString(bitstring_fp) 
-        rdkit_centroid_dict[ccluster] = rdkit_fp
+    files = np.genfromtxt(batch_partitions, dtype=str)
     
-    path = os.path.dirname(batch_centroids) + '/'
-    print(f'Saving the new centroids of the centroid clustering'
-            f' to {path}{outname}')
-    with open(path + outname + '_rdkit.pkl','wb') as f:
-        pickle.dump(rdkit_centroid_dict, f)
-    
-    # Get a dictionary of dictionaries with [partition][p_clusterid] = [c_clusterid]
-    pcluster_to_ccluster = {}
-    for i, ccluster in enumerate(clusters_labels):
-        partition, pcluster = total_ids[i]
-        if partition not in pcluster_to_ccluster:
-            pcluster_to_ccluster[partition] = {}
-            pcluster_to_ccluster[partition][pcluster] = ccluster
-        else:
-            pcluster_to_ccluster[partition][pcluster] = ccluster
+    with Pool(num_workers) as pool:
+        centroid_dfs = pool.map(process_file, files)
 
-    print(f'Adding the centroid cluster id to molecules on each partition')
-    for file in files:
-        partition = os.path.basename(file.replace('.pkl',''))
-        df = pd.read_csv(file.replace('.pkl','.csv'))
-        pclusters = df['partition_cluster'].to_list()
-        cclusters = []
-        for pcluster in pclusters:
-            ccluster = pcluster_to_ccluster[partition][pcluster]
-            cclusters.append(ccluster)
-        df['centroid_cluster'] = cclusters
-        df.to_csv(file.replace('.pkl','.csv')) 
+    centroid_df = pd.concat(centroid_dfs, ignore_index=True)
+    file_id = 'centroid dataframe'
 
-def get_object_memory(obj):
-    mem = sys.getsizeof(obj)
-    mem = mem / (1024 ** 3)
-    return mem
+    print(f'Total number of centroids to cluster: {len(centroid_df)}')
+    birch_centroid_labels, birch_cluster_labels = get_bitbirch_clusters(centroid_df, file_id)
 
-def get_bitbirch_centroid_clusters(fps):
-    
-    mem_fps = get_object_memory(fps)
-    print(f'Memory occupied by centroid fingerprints: {mem_fps}')
+    print(f'Adding cluster ID and centroid to: {file_id}')
+    centroid_df['centroid_cluster'] = birch_cluster_labels
+    centroid_df['centroid_centroid'] = birch_centroid_labels
+    centroid_df['centroid_cluster'] = centroid_df['centroid_cluster'].fillna(-100)
+    centroid_df['centroid_centroid'] = centroid_df['centroid_centroid'].fillna(-100)
+    centroid_df['centroid_cluster'] = np.array(centroid_df['centroid_cluster']).astype(int)
+    centroid_df['centroid_centroid'] = np.array(centroid_df['centroid_centroid']).astype(int)
 
-    bitbirch = bb.BitBirch(branching_factor=50, threshold=0.65)
-    print(f'Calculating clusters of centroids:')
-    bitbirch.fit(fps)
+    with Pool(num_workers) as pool:
+        update = partial(update_file, centroid_df=centroid_df)
+        pool.map(update, files)
 
-    centroids = bitbirch.get_centroids()
-    centroids_dict = {}
-    for i, centroid in enumerate(centroids):
-        centroids_dict[i] = centroid
-
-    cluster_list = bitbirch.get_cluster_mol_ids()
-    print(f'Number of clusters of centroids: {len(cluster_list)}')
-
-    print(f'Saving clusters:')
-    n_centroids = len(fps)
-    cluster_labels = [0] * n_centroids
-    for cluster_id, indices in enumerate(cluster_list):
-        for idx in indices:
-            cluster_labels[idx] = cluster_id
-
-    return centroids_dict, cluster_labels
 
 if __name__ == '__main__': 
     import argparse
 
-    parser = argparse.ArgumentParser(description='Script that clusters the centroid fingerprints stored in a pkl file.')
+    parser = argparse.ArgumentParser(description='Script that clusters the centroids of the initial partition clustering.')
 
     requiredArguments = parser.add_argument_group('Required Arguments')
-    requiredArguments.add_argument('--batch_centroids',
-                                   help='Batch file with the paths to the pickle files with the centroid fingerprints of its corresponding partition clustering',
+    requiredArguments.add_argument('--batch_partitions',
+                                   help='Batch file with the paths to the csv files with the real centroid of its corresponding partition clustering',
                                    required=True)
-    requiredArguments.add_argument('--outname',
-                                   help='Outname for pkl file of centroids of the centroid clustering',
+    requiredArguments.add_argument('--cores',
+                                   type=int,
+                                   help='Number of CPUs to parallelise on',
                                    required=True)
     
     args = parser.parse_args()
+    
+    # Load arguments
+    batch_partitions = args.batch_partitions
+    cores = args.cores
 
-    batch_centroids = args.batch_centroids
-    outname = args.outname
-
-    centroid_clustering(batch_centroids, outname)
+    centroid_clustering(batch_partitions, cores)
