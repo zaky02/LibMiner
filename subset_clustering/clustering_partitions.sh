bf7425e13622a960e930c32b7f60d286bd3f905c
Zaky02 <isaac.soul@alumni.esci.upf.edu>
2025-02-13 10:30:20 +0100

    Modified clustering partitions job file

--- subset_clustering/clustering_partitions.sh
+++ subset_clustering/clustering_partitions.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-#SBATCH --job-name=clustering
+#SBATCH --job-name=ZINC_clustering1
 #SBATCH --account=bsc72
 #SBATCH --chdir=.
-#SBATCH --output=logs/cluster/%j.out
-#SBATCH --error=logs/cluster/%j.err
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=28
 #SBATCH --time=48:00:00
 #SBATCH --qos=gp_bscls
 
 module purge
-ml bsc/1.0
+ml intel/2023.2.0
+ml cmake/3.25.1
+ml impi/2021.10.0
+ml mkl/2023.2.0
+ml miniconda/24.1.2
 ml anaconda
 
-source activate ProtSeq2StrucAlpha
+eval "$(conda shell.bash hook)"
+source activate /gpfs/projects/bsc72/conda_envs/MolecularAnalysis
 
-srun /home/bsc/bsc072876/.conda/envs/ProtSeq2StrucAlpha/bin/python clustering.py
+time python -u clustering_partitions.py --batch_partitions /gpfs/projects/bsc72/Libraries4DSD/ZINC20_batch_1 --cores 28 > ZINC_part_clustering.out 2>&1
