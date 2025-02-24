a5aa556f19ded934f5da16accced739af9117338
Zaky02 <isaac.soul@alumni.esci.upf.edu>
2025-02-24 10:32:55 +0100

    __pycache__ removed

--- old_scripts/get_subset.py
+++ old_scripts/get_subset.py
@@ -1,89 +1,147 @@
-import argparse
-import os 
-import glob
-import progressbar
-import pickle
-from multiprocessing import Pool
+import os
+import sys
 import pandas as pd
-from rdkit.Chem import rdFingerprintGenerator
+import numpy as np
+import pickle
+import random
+from multiprocessing import Pool, Manager
 from rdkit import Chem
+from rdkit.Chem import rdFingerprintGenerator, AllChem
+from rdkit.DataStructs import TanimotoSimilarity, BulkTanimotoSimilarity
+import psutil
 
-def get_info_db(input_dir):
-    files = glob.glob('%s/*' % input_dir)
-    total_molecules = 0
-    bar = progressbar.ProgressBar(maxval=len(files)).start()
-    for i, file in enumerate(files):
-        bar.update(i)
-        molecules = os.popen(f'wc -l < {file}').read().strip()
-        molecules = int(molecules)-1
-        total_molecules += molecules
-    bar.finish()
-    return len(files), total_molecules
-
-def compute_fp(smiles, fpgen):
-    """
-    Compute the Morgan fingerprint for a given smiles
-    """
-    # some smiles might have invalid smiles
-    try:
-        mol = Chem.MolFromSmiles(smiles)
-        if mol is None:
-            return None
-        fp = fpgen.GetSparseCountFingerprint(mol)
-        return(fp)
-    except Exception as e:
-        print(f'Error processing SMILES {smiles}: {e}')
-        return None
-
-
-def process_file(file):
-    """
-    Process a single file and compute the fingerprints for each SMILE string
-    """
-    try:
-        process_id = os.getpid()
-        print(f'[Process {process_id}] Processing file: {file}')
-
-        fpgen = rdFingerprintGenerator.GetMorganGenerator(radius=4)
-
-        df = pd.read_csv(file)
-        smiles_list = df['SMILES'].tolist()
-        ids_list = df['ID'].tolist()
-        fps = [compute_fp(smiles, fpgen) for smiles in smiles_list]
-
-        # create a dicctionary where the keys are the ids and the values the fingerprints
-        fps_dict = dict(zip(ids_list, fps))
-        
-        with open(file.replace('.csv','.pkl'), 'wb') as f:
-            pickle.dump(fps_dict, f)
-    except Exception as e:
-        print(f'Error processing file {file}: {e}')
-
-def process_directory(input_dir, num_workers):
-    """
-    Process files in the input_dir in parallel
-    """
-    files = glob.glob('%s/*' % input_dir)
+def process_batch(batch_partitions, num_workers, cfp, n, min_mol, outname):
     
-    with Pool(num_workers) as pool:
-        pool.map(process_file, files)
+    files = np.genfromtxt(batch_partitions, dtype=str)
+    
+    with open(cfp, 'rb') as f:
+        cfp = pickle.load(f)
 
+    with Manager() as manager:
+        shared_cfp = manager.dict(cfp)
+        #cluster_mols_dict = manager.dict({key: [[], []] for key in shared_cfp})
+        #cluster_mols_dict = {key: [[], []] for key in shared_cfp}
+        
+        with Pool(num_workers) as pool:
+            # Generate tasks for each multiprocess in pool
+            tasks = [(shared_cfp, part_file, n, min_mol, outname) for part_file in files]
+
+            results = pool.starmap(get_subset, tasks)
+        #print(results)
+    exit()
+    #merge_and_select_representatives(outname, n)
+
+def get_subset(cfp, part_file, n, min_mol, outname):
+    
+    process_id = os.getpid()
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+    print(f"[PROCESS {process_id}] Processing file: {part_file}")
+    print(f"Process {os.getpid()} - Memory Usage: {mem_info.rss / 1024 ** 2:.2f} MB")
+
+    part_df = pd.read_csv(part_file)
+    representatives = []
+  
+    cluster_mols_dict = {key: [[], []] for key in cfp.keys()}
+    for cluster_id, centroid_fp in cfp.items():
+        #if cluster_id > 5: break
+        # Keys: final cluster ID
+        # Values: fingerprints (numpy darray)
+       
+        #print(f"[PROCESS {process_id}] Retriving set of molecules belonging to ccluster")
+        set_mols = part_df[part_df['centroid_cluster'] == cluster_id]
+        if len(set_mols) == 0:
+            continue
+        ids = set_mols['ID'].to_list()
+        smiles = set_mols['SMILES'].to_list()
+        del set_mols
+        
+        #if len(set_mols) < min_mol:
+        #    continue
+
+        #print(f"[PROCESS {process_id}] Retrieving fingerprints of the ccluster molecules")
+        mols = [Chem.MolFromSmiles(smile) for smile in smiles]
+        fpgen = rdFingerprintGenerator.GetMorganGenerator(radius=4, fpSize=1024)
+        fps = [fpgen.GetFingerprint(mol) for mol in mols]
+        
+        #print(f"[PROCESS {process_id}] Retriving most similar molecule to the centroid of the ccluster")
+        similarities = BulkTanimotoSimilarity(centroid_fp, fps)
+        idx = np.argmax(similarities)
+        cluster_mols_dict[cluster_id][0].append((str(ids[idx]), smiles[idx]))
+        cluster_mols_dict[cluster_id][1].append(similarities[idx])
+        cluster_mols_dict[cluster_id].append(len(mols))
+    mem_dict = get_object_memory(cluster_mols_dict)
+    print(mem_dict, len(cluster_mols_dict.keys()))
+    return cluster_mols_dict
+        #fps = [AllChem.GetMorganFingerprintAsBitVect(mol, radius=4, nBits=1024) for mol in mols] 
+        #set_mols.loc[:, 'fingerprint'] = [AllChem.GetMorganFingerprintAsBitVect(mol, radius=4, nBits=1024) for mol in mols]
+        #X = np.array([list(fp) for fp in set_mols['fingerprint']])
+         
+        
+        #pop_counts = np.sum(X, axis=1)
+        #a_centroid = np.dot(X, values)
+        #
+        #sims_med = a_centroid / (pop_counts + np.sum(values) - a_centroid)
+        #sim_mol = np.argmax(sims_med)
+        
+        #print(f"[PROCESS {process_id}] Storing n representatives of the DB")
+        #representatives.append(set_mols.iloc[[sim_mol]])
+        #representatives_df = pd.concat(representatives, ignore_index=True)
+        #representatives_df.to_csv(f"{outname}_{process_id}_results.csv", index=False)
+
+def get_object_memory(obj):
+    mem = sys.getsizeof(obj)
+    mem = mem / (1024 ** 2)
+    return mem
+
+def merge_and_select_representatives(outname, n):
+    temp_files = [f for f in os.listdir() if f.endswith("_results.csv")]
+    merged_df = pd.concat([pd.read_csv(f) for f in temp_files], ignore_index=True)
+
+    merged_df = merged_df.drop_duplicates(subset=['centroid_cluster'])
+    merged_df = merged_df.drop(columns=['fingerprint'])
+
+    n_representatives = merged_df.sample(n=min(n, len(merged_df)))
+    n_representatives.to_csv(f"{outname}_final.csv", index=False)
+
+    for f in temp_files:
+        os.remove(f)
 
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Get a subset of cluster representatives')
+    requiredArguments = parser.add_argument_group('Required Arguments')
+    requiredArguments.add_argument('--cfp',
+                                   help='Pickle file with fps of the centroids',
+                                   required=True)
+    requiredArguments.add_argument('--batch_partitions',
+                                   help='Batch file with the paths to the DB partitions'
+                                        ' updated with the pcluster and ccluster ids',
+                                    required=True)
+    requiredArguments.add_argument('-n',
+                                   type=int,
+                                   help='Number of cluster representatives for the subset',
+                                   required=True)
+    requiredArguments.add_argument('--cores',
+                                   type=int,
+                                   help='Number of cores to parallelise',
+                                   required=True)
+    parser.add_argument('--min_mol',
+                        type=int,
+                        help='Minimum number of molecules for a cluster to be'
+                             'considered for representative extraction',
+                        default=10)
+    parser.add_argument('--outname',
+                        help='Name of the csv file storing the representative molecules')
+    
+    args = parser.parse_args()
 
-     parser = argparse.ArgumentParser(description='')
-     parser.add_argument('--db',
-                         type=str,
-                         help='')
-     parser.add_argument('--size',
-                         type=str,
-                         help='')
-     parser.add_argument('--cores', type=int, default=32, help='')
-     
-     args = parser.parse_args()
-     
-     #partitions, molecules = get_info_db(input_dir=args.db)
+    cfp = args.cfp
+    batch_partitions = args.batch_partitions
+    n = args.n
+    min_mol = args.min_mol
+    outname = args.outname
+    cores = args.cores
 
-     #process_directory(args.db, args.cores)
-
+    process_batch(batch_partitions, cores, cfp, n, min_mol, outname)
