8594f5dee75ff23091cdafd1526438e93bd469db
etiur <ruite.xiang@gmail.com>
2025-12-09 16:52:23 +0100

    move rename partition to utils

--- clean/processing.py
+++ clean/processing.py
@@ -42,7 +42,7 @@ blocker = rdBase.BlockLogs()
 
 PEPTIDE_SMARTS = "[NX3:1][CX3:2](=[OX1:20])[CX4:3][NX3:4][CX3:5](=[OX1:21])" # dipeptide motif
 Q = Chem.MolFromSmarts(PEPTIDE_SMARTS)
-unsatu = Chem.MolFromSmarts('[CD2;R0]=[CD2;R0][CD2;R0]=[CD2;R0][CD2;R0]=[CD2;R0][CD2;R0]=[CD2;R0][CD2;R0]=[C;R0]')
+unsatu = Chem.MolFromSmarts('[CD2;R0]=[CD2;R0][CD2;R0]=[CD2;R0][CD2;R0]=[CD2;R0][CD2;R0]=[CD2;R0]')
 
 tokenizer = re.compile(
         r'(\[[^\]]+\]|Br?|Cl?|N|O|S|P|F|I|B|C|b|c|n|o|s|p|\(|\)|\.|\=|\#|\-|\+|\\\\|\/|\:|\~|\@|\?|>>?|\*|\$|\%[0-9]{2}|[0-9])'
@@ -156,7 +156,7 @@ def normalize_smiles(smi: str) -> str | None:
     
     try:
         with dm.without_rdkit_log():
-            mol = dm.to_mol(smi, ordered=False)
+            mol = dm.to_mol(smi, ordered=True)
             if mol is None:
                 return None
             
@@ -335,42 +335,6 @@ def write_db_by_hac(db_id: str, pattern: list[str], output_folder: Path,
 
     print(f"DB {db_id} written to {output_folder}")
 
-
-def rename_partitions(output_folder: Path):
-    """
-    Adjust renaming logic for new database structure:
-    - Remove db_id level
-    - Rename HAC folders from HAC=value to HAC_value
-    - Rename parquet files to HAC_value_dbid_X.parquet
-    """
-    output_folder = Path(output_folder)
-
-    # Traverse db_id folders
-    for db_folder in output_folder.iterdir():
-        if not db_folder.is_dir():
-            continue
-        # Inside each db_id, look for HAC=* folders
-        for hac_folder in db_folder.glob("HAC=*"):
-            hac_value = hac_folder.name.split("=")[-1]
-            if  not int(hac_value):
-                new_hac_folder = output_folder / f"wrong_HAC_{hac_value}"
-            else:
-                new_hac_folder = output_folder / f"HAC_{hac_value}"
-                
-            new_hac_folder.mkdir(exist_ok=True, parents=True)
-
-            # Move and rename parquet files
-            for i, parquet_file in enumerate(hac_folder.glob("*.parquet"), start=1):
-                new_name = new_hac_folder / f"HAC{hac_value}_db{db_folder.name}_{i:02d}.parquet"
-                parquet_file.rename(new_name)
-            # Remove old HAC folder
-            if not any(hac_folder.iterdir()):
-                hac_folder.rmdir()
-
-        # Remove old db_id folder
-        if not any(db_folder.iterdir()):
-            db_folder.rmdir()
-
            
 def check_files(pattern: list[str], db_id: str, use_cols: tuple[str] = ("ID", "SMILES")) -> list[str]:
     """
@@ -433,7 +397,6 @@ def main():
             with open(progress, "a") as f:
                 f.write(f"DB {db_id} done\n")
                 
-        rename_partitions(out_path)
             
         end = time.perf_counter()
         print(f"Initial cleaning completed in {end - start:.2f} seconds")    
